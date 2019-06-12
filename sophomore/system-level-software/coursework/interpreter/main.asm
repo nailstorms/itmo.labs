7e7670b9025732597d10a96c1e3178605797d0db
nailstorms <nailstorm@protonmail.ch>
2019-06-12 15:01:43 +0300

    sls cw: reverting all previous commits to one
    proof that there WERE commits - https://puu.sh/DEYBu/809c3bcc16.png
    basically I did this because there was pretty much no logic behing them, all being stupid mistakes and one-line fixes

--- sophomore/system-level-software/coursework/interpreter/main.asm
+++ sophomore/system-level-software/coursework/interpreter/main.asm
@@ -1,10 +1,45 @@
+<<<<<<< HEAD
 global _start
+%include "qol-macros.inc"
+%include "lib.inc"
 
-%include 'dictionary.inc'
-
+=======
+>>>>>>> parent of 9034aab... sls cw: testing, add makefile
 %define pc     r15 
 %define w      r14
 %define rstack r13
+
+; ---------------------------------------------------------------
+
+<<<<<<< HEAD
+section .text
+
+%include "dictionary.inc"
+%include "colon.inc"
+
+; ---------------------------------------------------------------
+
+section .bss
+
+resq 1023
+rstack_start: resq 1
+user_mem: resq 65536
+
+; ---------------------------------------------------------------
+
+section .data 
+last_word: dq __prev_word_
+stack_start: dq 0
+dp: dq user_mem
+ 
+=======
+section .data
+
+err_msg: db "Not found: ", 0
+
+last_word: dq __prev_word_
+here: dq dictionary
+pointer: dq mem   
 
 ; ---------------------------------------------------------------
 
@@ -13,44 +48,24 @@ section .bss
 resq 1023
 rstack_start: resq 1
 
-input_buf: resb 1024
-user_dict:  resq 65536
+dictionary: resq 65536                  
+mem: resq 65536                  
+state: resq 1 
+word_buf: resb 1024 
 
-user_mem: resq 65536
-
-state: resq 1
-
+>>>>>>> parent of 9034aab... sls cw: testing, add makefile
 ; ---------------------------------------------------------------
 
-section .data 
-last_word: dq __prev_word_
-here: dq user_dict
-dp: dq user_mem
-
-; ---------------------------------------------------------------
-
-section .rodata
-no_such_word: db "No such word", 10, 0
- 
-; ---------------------------------------------------------------
-
-section .text
-
-%include "dictionary.inc"
 
 global _start
 
-program:
-    dq xt_lit, 8, xt_lit, 2, xt_div, xt_dot, xt_lit, 6, xt_lit, 1, xt_or, xt_dot, xt_bye
-
-_start:
+_start: 
     mov rstack, rstack_start
     mov [stack_start], rsp
-    mov pc, program
-    jmp next    
+    mov pc, init
 
 next:
-    mov w, [pc]
+    mov w, pc
     add pc, 8
     mov w, [w]
     jmp [w]
